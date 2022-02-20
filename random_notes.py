0e5b21cf9656b82bbffdc1cf0d1d9e966263c08c
joycehhhhhh <95155016+joycehhhhhh@users.noreply.github.com>
2022-02-20 16:45:38 +0800

    Update random_notes.py

--- random_notes.py
+++ random_notes.py
@@ -1,1 +1,16 @@
-#
+#standardize the data
+cols_to_scale=['column1','column2']
+from sklearn.preprocessing import MinMaxScaler
+scaler=MinMaxScaler
+df1[cols_to_scale]=scaler.fit_transform(df1[cols_to_scale])
+df.sample(3)
+
+#one hot coding for data preparation
+df2=pd.get_dummies(data=df1, columns=['column1', 'column2'])
+df2.columns
+
+#see unique values in each column
+for col in df2:
+	print(f'{col}:{df1[col].unique()'})
+
+
